1eff2a50df4841956ebfa638cc29562c9a552f2e
rishipatel2107 <113211480+rishipatel2107@users.noreply.github.com>
2022-11-25 13:00:06 -0700

    lab8
    This is a security demo

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author RJ
+ */
+public class AdminFilter implements Filter {
+    
+    
+    
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+            String role = (String) httpRequest.getSession().getAttribute("role");
+            
+            if(role != null && role.equals("admin")){
+                chain.doFilter(request, response);
+                return;
+            }
+            
+            httpResponse.sendRedirect("notes");
+        
+        }
+        
+        
+    
+}
